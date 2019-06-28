700ae6e4b32cdcf81eb17228e00711afe4bf4b82
Paweł Panek <pawel.panek@nowatel.com>
2019-06-28 13:05:43 +0200

    Added tests

--- src/main/java/pablo/todo/App.java
+++ src/main/java/pablo/todo/App.java
@@ -3,6 +3,7 @@ package pablo.todo;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
@@ -10,82 +11,100 @@ import javafx.stage.Stage;
 
 public class App extends Application {
 
-    private ListView<String> tasksView = new ListView<>();
-    private TextField contentView = new TextField();
-    private ObservableList<String> tasks = FXCollections.observableArrayList(
-            "Task 1", "Task 2", "Task 3"
-    );
-
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("ToDo");
 
-        BorderPane listPane = new BorderPane();
+        Parent root = new ToDoPane();
+        Scene scene = new Scene(root, 700, 400);
+        primaryStage.setScene(scene);
 
-        tasksView.setItems(tasks);
-        tasksView.setOnMouseClicked( event -> {
-            String text = tasksView.getSelectionModel().getSelectedItem();
-            contentView.setText(text);
-
-        });
-        listPane.setLeft(tasksView);
-        listPane.setCenter(contentView);
-
-        setupRightPane(listPane);
-
-        StackPane root = new StackPane();
-        root.getChildren().add(listPane);
-        primaryStage.setScene(new Scene(root, 700, 400));
         primaryStage.show();
     }
 
-    private void setupRightPane(BorderPane root) {
-        HBox rightPane = new HBox(2);
+    public static class ToDoPane extends StackPane {
 
-        Pane spacer = new Pane();
-        HBox.setHgrow(spacer, Priority.ALWAYS);
-        spacer.setMinSize(100, 1);
+        private ListView<String> tasksView = new ListView<>();
+        private TextField contentView = new TextField();
+        private ObservableList<String> tasks = FXCollections.observableArrayList(
+                "Task 1", "Task 2", "Task 3"
+        );
 
-        TextArea newTaskView = new TextArea();
-        newTaskView.setPrefHeight(50);
-        newTaskView.setPrefWidth(50);
+        ToDoPane() {
+            super();
+            setupScene();
+        }
 
-        Button addBtn = new Button();
-        addBtn.setText("Add task");
-        addBtn.setOnAction( event -> {
-            if (!newTaskView.getText().isEmpty()) {
-                tasks.add(newTaskView.getText());
-                newTaskView.setText("");
-            } else {
-                Alert alert = new Alert(Alert.AlertType.INFORMATION);
-                alert.setTitle("Empty task");
-                alert.setHeaderText("Error");
-                alert.setContentText("You can't add task without text.");
-                alert.showAndWait();
-            }
-        });
+        private void setupScene() {
+            BorderPane listPane = new BorderPane();
 
-        Button deleteBtn = new Button();
-        deleteBtn.setText("Delete task");
-        deleteBtn.setOnAction( event -> {
-            int index = tasksView.getSelectionModel().getSelectedIndex();
-            if (index > -1) {
-                tasksView.getItems().remove(index);
-                contentView.setText("");
-            } else {
-                Alert alert = new Alert(Alert.AlertType.INFORMATION);
-                alert.setTitle("Select task");
-                alert.setHeaderText("Error");
-                alert.setContentText("You have to select task to delete.");
-                alert.showAndWait();
-            }
-        });
+            tasksView.setId("tasksView");
+            contentView.setId("contentView");
+            tasksView.setItems(tasks);
+            tasksView.setOnMouseClicked(event -> {
+                String text = tasksView.getSelectionModel().getSelectedItem();
+                contentView.setText(text);
 
-        rightPane.getChildren().addAll(spacer, newTaskView, addBtn, deleteBtn);
-        root.setRight(rightPane);
+            });
+            listPane.setLeft(tasksView);
+            listPane.setCenter(contentView);
+
+            setupRightPane(listPane);
+
+            getChildren().add(listPane);
+        }
+
+        private void setupRightPane(BorderPane root) {
+            HBox rightPane = new HBox(2);
+
+            Pane spacer = new Pane();
+            HBox.setHgrow(spacer, Priority.ALWAYS);
+            spacer.setMinSize(100, 1);
+
+            TextArea newTaskView = new TextArea();
+            newTaskView.setId("newTaskView");
+            newTaskView.setPrefHeight(50);
+            newTaskView.setPrefWidth(50);
+
+            Button addBtn = new Button();
+            addBtn.setId("addBtn");
+            addBtn.setText("Add task");
+            addBtn.setOnAction(event -> {
+                if (!newTaskView.getText().isEmpty()) {
+                    tasks.add(newTaskView.getText());
+                    newTaskView.setText("");
+                } else {
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setTitle("Empty task");
+                    alert.setHeaderText("Error");
+                    alert.setContentText("You can't add task without text.");
+                    alert.showAndWait();
+                }
+            });
+
+            Button deleteBtn = new Button();
+            deleteBtn.setId("deleteBtn");
+            deleteBtn.setText("Delete task");
+            deleteBtn.setOnAction(event -> {
+                int index = tasksView.getSelectionModel().getSelectedIndex();
+                if (index > -1) {
+                    tasksView.getItems().remove(index);
+                    contentView.setText("");
+                } else {
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setTitle("Select task");
+                    alert.setHeaderText("Error");
+                    alert.setContentText("You have to select task to delete.");
+                    alert.showAndWait();
+                }
+            });
+
+            rightPane.getChildren().addAll(spacer, newTaskView, addBtn, deleteBtn);
+            root.setRight(rightPane);
+        }
     }
 
     static void run(String[] args) {
         launch(args);
     }
-}
+    }
--- src/testIntegration/java/pablo/todo/AppITest.java
+++ src/testIntegration/java/pablo/todo/AppITest.java
@@ -1,6 +1,72 @@
 package pablo.todo;
 
-class AppITest {
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseButton;
+import javafx.stage.Stage;
+import org.junit.After;
+import org.junit.Test;
+import org.testfx.api.FxToolkit;
+import org.testfx.framework.junit.ApplicationTest;
+
+import java.util.concurrent.TimeoutException;
+
+import static org.junit.Assert.assertEquals;
+import static org.testfx.api.FxAssert.verifyThat;
+import static org.testfx.matcher.control.LabeledMatchers.hasText;
+
+public class AppITest extends ApplicationTest {
 
 
+
+    @Override
+    public void start(Stage stage){
+        Parent root = new App.ToDoPane();
+        Scene scene = new Scene(root, 700, 400);
+        stage.setScene(scene);
+        stage.show();
+        stage.toFront();
+    }
+
+    @Test
+    public void addBtnShouldContainText() {
+        verifyThat("#addBtn", hasText("Add task"));
+    }
+
+    @Test
+    public void deleteBtnShouldContainText() {
+        verifyThat("#deleteBtn", hasText("Delete task"));
+    }
+
+    @Test
+    public void tasksListShouldContainThreeTasks() {
+        ListView<String> tasksView = lookup("#tasksView").query();
+
+        int itemsCount = tasksView.getItems().size();
+
+        assertEquals("TasksView should contain 3 elements given: " + itemsCount, 3, itemsCount);
+    }
+
+    @Test
+    public void insertNewTaskThenVerifyIfNewTaskIsAdded() {
+        ListView<String> tasksView = lookup("#tasksView").query();
+        TextArea newTaskView = lookup("#newTaskView").query();
+
+        newTaskView.setText("Test");
+        clickOn("#addBtn");
+        int itemsCount = tasksView.getItems().size();
+
+        assertEquals("TasksView should contain 4 elements given: " + itemsCount, 4, itemsCount);
+    }
+
+    @After
+    public void tearDown() throws TimeoutException {
+        FxToolkit.hideStage();
+        release(new KeyCode[] {});
+        release(new MouseButton[] {});
+    }
 }
